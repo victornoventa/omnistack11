6c858d051ca691a34c8c80167a13f31fbc029246
victor.noventa <victor.noventa@avenuecode.com>
2020-03-26 14:05:54 -0300

    React application for Be The Hero

---
+++ frontend/src/routes.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
+import Logon from "./pages/Logon";
+import Register from "./pages/Register";
+import Profile from "./pages/Profile";
+import NewIncident from "./pages/NewIncident";
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route path="/" component={Logon} exact />
+                <Route path="/register" component={Register} exact />
+                <Route path="/profile" component={Profile} exact />
+                <Route path="/incidents/new" component={NewIncident} exact />
+            </Switch>
+        </BrowserRouter>
+    );
+};
+
+export default Routes;
